55cf3d096f0816f428ec129003f8d022c7f892d2
dabit3 <dabit3@gmail.com>
2021-09-01 16:32:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bored Ape Yach Club Subgraph
+
+![Bored Ape Yach Club Subgraph](header.png)
+
+This is a subgraph that enables the indexing and querying of data from the Bored Ape Yacht Club and Mutant Ape Yacht Club NFTs / smart contracts
+
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,53 +1,211 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
+  Token as TokenContract,
   Transfer as TransferEvent
-} from "../generated/Token/Token"
-import {
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/schema"
+ } from "../generated/Token/Token"
+ 
+ import {
+  Token, User
+ } from '../generated/schema'
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
+ import { log, ipfs, json } from '@graphprotocol/graph-ts'
 
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
-}
+ let baseHash = "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq"
+ let fullIPFSURI = "ipfs.io/ipfs/" + baseHash + "/"
+ 
+ export function handleTransfer(event: TransferEvent): void {
+  
+  var token = Token.load(event.params.tokenId.toString());
+  if (!token) {    
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token.tokenID = event.params.tokenId;
+    token.createdAtTimestamp = event.block.timestamp;
+  }
+  let tokenContract = TokenContract.bind(event.address);
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
+  let baseURI = tokenContract.baseURI()
+  let contentURI = tokenContract.tokenURI(event.params.tokenId);
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
+  if (baseURI.includes('https')) {
+    baseURI = fullIPFSURI;
+  } else if (baseURI.includes('ipfs')) {
+    let hash = baseURI.split('ipfs://').join('')
+    baseURI = "ipfs.io/ipfs" + hash
+  }
+
+  if (contentURI.includes('https')) {
+    contentURI = fullIPFSURI + event.params.tokenId.toString();
+  } else {
+    let hash = contentURI.split('ipfs://').join('')
+    contentURI = "ipfs.io/ipfs/" + hash + event.params.tokenId.toString();
+  }
+
+  token.contentURI = contentURI;
+  token.baseURI = baseURI;
+
+  if (contentURI != "") {
+    let hash = contentURI.split('ipfs.io/ipfs/').join('')
+    let data = ipfs.cat(hash)
+    
+    if (!data) return
+    let value = json.fromBytes(data!).toObject()
+
+    if (data != null) {
+      let attributes = value.get('attributes').toArray()
+
+      for (let i = 0; i < attributes.length; i++) {
+        let item = attributes[i].toObject()
+        let trait = item.get('trait_type').toString()
+        let value = item.get('value').toString()
+
+        if (trait == "Mouth") {
+          token.mouth = 'Bored Cigarette'
+        }
+
+        if (trait == "Eyes") {
+          token.eyes = value
+        }
+
+        if (trait == "Background") {
+          token.background = value
+        }
+
+        if (trait == "Hat") {
+          token.hat = value
+        }
+
+        if (trait == "Clothes") {
+          token.clothes = value
+        }
+
+        if (trait == "Fur") {
+          token.fur = value
+        }
+
+        if (trait == "Earring") {
+          token.earring = value
+        }
+      }
+    }
+  }
+
+  token.owner = event.params.to.toHexString();
+  token.save();
+ 
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
+ }
+
+// import {
+//   Token as TokenContract,
+//   Transfer as TransferEvent
+//  } from "../generated/Token/Token"
+ 
+//  import {
+//   Token, User
+//  } from '../generated/schema'
+
+//  import { log, ipfs, json } from '@graphprotocol/graph-ts'
+
+//  let ipfsURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/"
+ 
+//  export function handleTransfer(event: TransferEvent): void {
+  
+//   var token = Token.load(event.params.tokenId.toString());
+//   if (!token) {    
+//     token = new Token(event.params.tokenId.toString());
+//     token.creator = event.params.to.toHexString();
+//     token.tokenID = event.params.tokenId;
+//     token.createdAtTimestamp = event.block.timestamp;
+//   }
+//   let tokenContract = TokenContract.bind(event.address);
+
+//   let baseURI = tokenContract.baseURI()
+//   let contentURI = tokenContract.tokenURI(event.params.tokenId);
+
+//   if (baseURI.includes('https')) {
+//     baseURI = ipfsURI;
+//   }
+
+//   if (contentURI.includes('https')) {
+//     contentURI = ipfsURI + event.params.tokenId.toString();
+//   }
+
+//   token.contentURI = contentURI;
+//   token.baseURI = baseURI;
+
+//   if (contentURI != "") {
+//     let hash = contentURI.split('ipfs://').join('')
+//     let data = ipfs.cat(hash)
+//     let value = json.fromBytes(data!).toObject()
+    
+//     log.info("data:::: {}", [data.toString()])
+//     log.info("hash:::: {}", [hash])
+
+//     if (data != null) {
+//       let attributes = value.get('attributes').toArray()
+
+//       for (let i = 0; i < attributes.length; i++) {
+//         let item = attributes[i].toObject()
+//         let trait = item.get('trait_type').toString()
+//         let value = item.get('value').toString()
+
+//         if (trait == "Mouth") {
+//           token.mouth = 'Bored Cigarette'
+//         }
+
+//         if (trait == "Eyes") {
+//           token.eyes = value
+//         }
+
+//         if (trait == "Background") {
+//           token.background = value
+//         }
+
+//         if (trait == "Hat") {
+//           token.hat = value
+//         }
+
+//         if (trait == "Clothes") {
+//           token.clothes = value
+//         }
+
+//         if (trait == "Fur") {
+//           token.fur = value
+//         }
+
+//         if (trait == "Earring") {
+//           token.earring = value
+//         }
+//       }
+
+//     }
+//   }
+
+//   token.owner = event.params.to.toHexString();
+//   token.save();
+ 
+//   let user = User.load(event.params.to.toHexString());
+//   if (!user) {
+//     user = new User(event.params.to.toHexString());
+//     user.save();
+//   }
+//  }
+
+
+ /*
+ logging info in working example
+
+ Hash
+ QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/271
+
+ data
+ data:::: {"image":"ipfs://QmVQsuBRpLbAXHVg183L6juFUyWoySf6vvrT7nRNwUtTMb","attributes":[{"trait_type":"Eyes","value":"Coins"},{"trait_type":"Mouth","value":"Phoneme L"},{"trait_type":"Hat","value":"Halo"},{"trait_type":"Background","value":"Orange"},{"trait_type":"Fur","value":"Dark Brown"}]}
+
+ non working example
+ data
+ data:::: {"image":"ipfs://QmSK1HJCiMuNahMyNBq7puEwBBUWjzjeCnQwCVgRaqkVJP","attributes":[{"trait_type":"Mouth","value":"Bored Unshaven"},{"trait_type":"Eyes","value":"Wide Eyed"},{"trait_type":"Fur","value":"Red"},{"trait_type":"Background","value":"Army Green"},{"trait_type":"Clothes","value":"Guayabera"}]}
+ */
